366871c3086d14b08f01f89d32debf254b0d52d4
Oleg <oleg.kurkov.85@gmail.com>
2022-11-17 15:48:03 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            string error = "Ошибка в БД";
+
+            try
+            {
+                Console.WriteLine("Блок Try начал свою работу!");
+                throw new Exception(error);
+            }
+
+            catch (Exception ex) when (ex.Message == error) /// делаем фильтрацию по соственному типу исключения
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex) when (ex is ArgumentNullException) /// делаем фильтрацию исключений
+            {
+                Console.WriteLine("Аргумент пустой!");
+            }
+
+            catch (Exception ex) when (ex is FileNotFoundException) /// делаем фильтрацию исключений
+            {
+                Console.WriteLine("Файл не найден");
+            }
+
+            catch (Exception ex) when (ex is PlatformNotSupportedException) /// делаем фильтрацию исключений
+            {
+                Console.WriteLine("Операция не поддерживается на текущей платформе");
+            }
+
+            catch (Exception ex) when (ex is IndexOutOfRangeException) /// делаем фильтрацию исключений
+            {
+                Console.WriteLine("Индекс находится за пределами границ массива");
+            }
+
+            finally
+            {
+                Console.WriteLine("Блок finally сработал!");
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ListFirstName = new List<string>(); /// создаем пустой список
+            string[] FirstName = new string[5]; /// создаем массив из 5 значений
+
+            for (int i = 0; i < FirstName.Length; i++)
+            {
+                Console.WriteLine("Введите фамелию {0}: ", i + 1);
+                string firstName = Console.ReadLine();
+                ListFirstName.Add(firstName);
+            }
+
+            FamilySorted familySorted = new FamilySorted(); /// создаем экземляр класса
+            familySorted.FamilySortedEvent += ShowNuber;
+
+            try /// проверяем на правильность ввденого формата
+            {
+                familySorted.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено не корректное значение");
+            }
+
+            Console.WriteLine();
+                
+            ListFirstName.Sort(); /// сортируем список
+            foreach (string fName in ListFirstName)
+            {
+                Console.WriteLine(fName);
+            }
+
+            Console.WriteLine();
+
+            ListFirstName.Reverse(); /// сортируем список в обратном порядке
+            foreach (string fName in ListFirstName)
+            {
+                Console.WriteLine(fName);
+            }
+
+            static void ShowNuber(int number)
+            {
+                switch (number)
+                {
+                    case 1:
+                        ListFirstName.Sort(); /// сортируем список
+                        foreach (string fName in ListFirstName)
+                        {
+                            Console.WriteLine(fName);
+                        }
+
+                        break;
+                    case 2: Console.WriteLine("2"); break;
+                }
+            }
+
+
+
+        }
+    }
+
+    class FamilySorted /// создаем новый класс
+    {
+        public delegate void FamilySortedDelegate(int number); /// создаем делегат , на ввод принимает число
+        public event FamilySortedDelegate FamilySortedEvent; /// реализовываем событие
+        public void Read() /// создаем метод, который читает введеное значение
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введете число 1 (сортировка А-Я), либо число 2 (сортировка Я-А): ");
+
+            int number = Convert.ToInt32(Console.ReadLine()); /// введеное значение от пользователя
+
+            if (number != 1 && number != 2) throw new FormatException(); /// правильно введены ли только числа 1 или 2
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            FamilySortedEvent?.Invoke(number);
+        }
+    }
+
+}
